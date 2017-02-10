05adaa67c77fa3869c75b2712505296414546aa4
Sage Hawk <SageHawk@sage.local>
2017-02-09 21:49:29 -0600

    Added Alert View

--- College Profile Builder/MasterViewController.swift
+++ College Profile Builder/MasterViewController.swift
@@ -38,9 +38,37 @@ class MasterViewController: UITableViewController {
     }
 
     func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        self.tableView.insertRows(at: [indexPath], with: .automatic)
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "College"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Location"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enrollment"
+            textField.keyboardType = UIKeyboardType.numberPad
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alert.addAction(cancelAction)
+            let insertAction = UIAlertAction(title: "Add", style: .default) { (action) in
+                let collegeTextField = alert.textFields![0] as UITextField
+                let locationTextField = alert.textFields![1] as UITextField
+                let enrollmentTextField = alert.textFields![2] as UITextField
+                guard let image = UIImage(named: collegeTextField.text!) else{
+                    print("missing \(collegeTextField.text!) image")
+                    return }
+                if let enrollment = Int(enrollmentTextField.text!) {
+                    let college = College(name: collegeTextField.text!,
+                                    location: locationTextField.text!,
+                                    enrollment: enrollment,
+                                    image: UIImagePNGRepresentation(image)!)
+                    self.objects.append(college)
+                    self.tableView.reloadData()
+                }
+            }
+            alert.addAction(insertAction)
+            present(alert, animated: true, completion: nil)
+        }
     }
 
     // MARK: - Segues
