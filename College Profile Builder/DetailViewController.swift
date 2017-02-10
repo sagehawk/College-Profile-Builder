7c7dab731f29e872f6a7d6f52346edc8bd7a3ee3
Sage Hawk <SageHawk@sage.local>
2017-02-09 21:34:18 -0600

    Connected UI objects to storyboard

--- College Profile Builder/DetailViewController.swift
+++ College Profile Builder/DetailViewController.swift
@@ -10,7 +10,34 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var collegeTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var enrollmentTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var detailItem: College? {
+        didSet {
+            func configureView() {
+                // Update the user interface for the detail item
+                if let college = self.detailItem {
+                    if collegeTextField != nil {
+                        collegeTextField.text = college.name
+                        locationTextField.text = college.location
+                        enrollmentTextField.text = String(college.enrollment)
+                        imageView.image = UIImage(data: college.image)
+                    }
+                }
+            }
+        }
+    }
+        @IBAction func onTappedSaveButton(_ sender: Any) {
+        if let college = self.detailItem {
+        college.name = collegeTextField.text!
+        college.location = locationTextField.text!
+        college.enrollment = Int(enrollmentTextField.text!)!
+        college.image = UIImagePNGRepresentation(imageView.image!)!
+        }
+        }
 
 
     func configureView() {
@@ -33,11 +60,7 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
+    
     }
 
 
